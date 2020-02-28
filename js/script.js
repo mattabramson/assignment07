9532d32297a464cdf74fa10907ca79c1b0fff9c2
mattabramson <60493857+mattabramson@users.noreply.github.com>
2020-02-28 18:37:57 -0500

    Refined text in HTML

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//Learn More about Recording
+let button = document.querySelector("#need");
+let list = document.querySelector("#list");
+
+list.style.display = "none";
+
+button.onclick = () => {
+  if (list.style.display === "none") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  };
+};
+
+//Bass Guitar Quiz
+let correct = document.querySelector("#boxer");
+
+correct.onclick = () => {
+  correct.style.background = "#FFAA3F";
+};
+
+//Synthesizer Photo Switch
+let daniel = document.querySelector("#daniel");
+
+synth.style.display = "none";
+
+daniel.onmouseover = () => {
+  if (synth.style.display === "none") {
+    daniel.style.display = "none";
+    synth.style.display = "block";
+  } else {
+    synth.style.display = "none";
+//Can't revert image back yet
+  }
+};
+
+daniel.addEventListener.mouseleave = (evt) => {
+  console.log(event)
+  console.log('Event triggered')
+}
